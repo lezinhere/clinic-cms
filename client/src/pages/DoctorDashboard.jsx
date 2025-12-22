6ddef922e701764430c2c6ede08e6ec7d60b7286
lezinhere <lezinhere@gmail.com>
2025-12-22 10:41:07 +0530

    Chore: Force Rebuild with real file change

--- client/src/pages/DoctorDashboard.jsx
+++ client/src/pages/DoctorDashboard.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+// Force Vercel Rebuild 2
 import { Link } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import { doctorApi } from "../api/doctor";
